bd07928d0389bdbc2c9da0eda8c5a95452bbe248
DruMoDev <dev.morera@gmail.com>
2025-03-13 21:46:09 +0100

    actualizados los componentes user-list con todos los usuarios y creado el user-card

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { UserCardComponent } from "../user-card/user-card.component";
+import { Component, inject } from '@angular/core';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-users-list',
   imports: [UserCardComponent],
   templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.css'
+  styleUrl: './users-list.component.css',
 })
 export class UsersListComponent {
+  usersList: User[] = [];
+  usersService = inject(UsersService);
 
+  async ngOnInit() {
+    try {
+      let response = await this.usersService.getUsers();
+      this.usersList = response.results
+      console.log(this.usersList);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
